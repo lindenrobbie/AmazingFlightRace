f2d568a2cf56d32b4b69c48b9872ad836060a191
Mika Poikonen <mikapoi@metropolia.fi>
2025-04-28 21:19:28 +0300

    Html sivut,css,js by mika

--- html/js/main.js
+++ html/js/main.js
@@ -1,9 +1,15 @@
 'use strict'
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("Manual").addEventListener("click", function() {
+        window.open("manual.html", "ManualPopup", "width=700,height=900");
+    });
+})
 
     document.addEventListener("DOMContentLoaded", function () {
         const soundButton = document.getElementById("Sounds"); //kuuntelee äänilogoa
         const music = document.getElementById("music");// hakee musiikki linkin html puolelta
 
+        music.loop = true
         soundButton.addEventListener("click", function () { //kuuntelee klikkiä
             if (music.paused) {
                 music.play();

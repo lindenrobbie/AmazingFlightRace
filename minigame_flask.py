6067dd877122f001958d4999faa0ff0866440832
EliasL329 <elias.lee@metropolia.fi>
2025-04-29 15:43:20 +0300

    korjasin minigame flaskia vielä kerran ja lisäsin kommentteja

--- minigame_flask.py
+++ minigame_flask.py
@@ -9,37 +9,40 @@ cors = CORS(app)
 
 @app.route('/minigame')
 def minigame():
-    icaoCodes = db_modules.getAirport('ident')
+    #getAirport hakee tietokannan "minigame" taulukosta satunnaisen "minigame_id,", "1" on haettujen kolumnien määrä
+    #eli hakee yhden satunnaisen minipelin icao-koodin
+    icaoCode = db_modules.getAirport('ident', 1)
+    airport1 = db_modules.db_command(f'select * from minigame where minigame_id = "{icaoCode[0][0]}"') #getairport palauttaa muodossa [["icao,"]]
 
-    airport1 = db_modules.db_command(f'select * from minigame where minigame_id = "{icaoCodes[0][0]}"')
-    answer1= {
+    #luo json muotoisen version tiedosta
+    answer= {
         "icao" : airport1[0][0],
         "question" : airport1[0][1],
         "options" : airport1[0][2],
         "answer" : airport1[0][3]
     }
 
-    airport2 = db_modules.db_command(f'select * from minigame where minigame_id = "{icaoCodes[1][0]}"')
-    answer2= {
-        "icao" : airport2[0][0],
-        "question" : airport2[0][1],
-        "options" : airport2[0][2],
-        "answer" : airport2[0][3]
-    }
-    answer = [answer1, answer2]
+    #muuttaa json:iksi ja lähettää osoitteeseen "http://127.0.0.1:3000/minigame", josta js:n puolella fetchataan tiedot
     json_answer = json.dumps(answer)
+
+    #palautus on muodossa: {"icao": "icao", "question": "minipelin tehtävä", "options": "minipelin 4 mahdollista vastausta", "answer": "oikea vastas / vastaukset, esim 1, jos ensimmäinen vastaus on oiken"}
     return json_answer
 
 
+#odottaa fetch() hakua js puolelta, jossa haetaan fetch(http://127.0.0.1:3000/minigame_results + game_id, icao, points)
 @app.route('/minigame_results')
 def results():
+    #kerää tiedot fetch() haun url:ista
     args = request.args
     game_id = args.get("id")
     icao = args.get("icao")
     points = args.get("points")
 
+    #siirtää url:ista kerätyt tiedot tietokantaan
     db_modules.db_command(f'UPDATE game SET player_score += {points} WHERE game_ID = "{game_id}"')
     db_modules.db_command(f'UPDATE minigame SET complete = 1 WHERE minigame_id = "{icao}"')
+    
+    #lopuksi ei kuitenkaan palauta mitään, tähän vois jkuitenkin laittaa palautuksena esim. "fetch succesfull" -merkkijonon, jos haluaa
 
 
 if __name__ == '__main__':

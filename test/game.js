eba5c8e835d3ae1931fe0d39317d1a1244ed6057
EliasL329 <elias.lee@metropolia.fi>
2025-04-30 18:34:02 +0300

    minipelin icao koodilla hakemisen testausta

--- test/game.js
+++ test/game.js
@@ -1,29 +1,19 @@
 'use strict';
 
-const url = 'http://127.0.0.1:3000/minigame'
-
-async function getData() {
+async function getMinigame(icao) {
 	try {
-		const response = await fetch('http://127.0.0.1:3000/minigame');
+		const response = await fetch(`http://127.0.0.1:3000/minigame?icao=${icao}`);
 		const data = await response.json();
-		document.querySelector('#data').innerHTML = Object.values(data);
 		return data;
 	} catch (error) {
 		console.log(error)
 		};	
 	};
 
-async function postData(url) {
-	const data =  await fetch(url);
-	const response = await data.json();
-	return response;
-
-};
-
-document.querySelector('#button').addEventListener('click', () => {
-	getData(url);
-})
-
-document.querySelector('#update').addEventListener('click', () => {
-	postData(url);
+	
+document.querySelector('#getData').addEventListener('submit', async (evt) => {
+	evt.preventDefault();
+	const input = document.querySelector('#icao').value;
+	const data = await getMinigame(input);
+	document.querySelector('#data').innerHTML = Object.values(data);
 })

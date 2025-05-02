5d3a3faa81fbb1342e666ad9ad8e5809607974f7
linde <robbie.linden@metropolia.fi>
2025-05-02 14:54:18 +0300

    choosen location update link nimen fiksaus

--- html/js/chooselocation.js
+++ html/js/chooselocation.js
@@ -1,9 +1,19 @@
-// map.js
-fetch('http://127.0.0.1:3000/coordinates')
-  .then(response => response.json())
-  .then(cities => {
+import { initFlightBackground } from './js/background.js';
+
+initFlightBackground('#game-background');
+
+async function loadMap() {
+  try {
+    const response = await fetch('http://127.0.0.1:3000/coordinates');
+    const cities = await response.json();
     console.log("Loaded cities:", cities);
+
     const map = L.map('map', { zoomControl: false });
+
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; OpenStreetMap & CartoDB',
+      maxZoom: 10
+    }).addTo(map);
 
     const markers = cities.map(city =>
       L.marker([city.lat, city.lon])
@@ -11,12 +21,11 @@ fetch('http://127.0.0.1:3000/coordinates')
         .bindPopup(city.name)
     );
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
-      attribution: '&copy; OpenStreetMap & CartoDB',
-      maxZoom: 10
-    }).addTo(map);
-
     const group = L.featureGroup(markers);
     map.fitBounds(group.getBounds().pad(0.3));
-  })
-  .catch(err => console.error("Error loading cities:", err));
+  } catch (err) {
+    console.error("Error loading map or cities:", err);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', loadMap);

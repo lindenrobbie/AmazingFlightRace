0b33fd9b4245b977a05b7dda55d3fa560a8fe685
Ville <ville.ahola@protonmail.com>
2025-05-02 11:52:19 +0300

    leaderboard sivun logiikka toimii mutta ulkoasu puuttuu

---
+++ html/js/leaderboard.js
@@ -0,0 +1,18 @@
+'use strict'
+
+
+async function playerdata() {
+  const data = await fetch("http://127.0.0.1:3000/scoreboard")
+  const data_json = await data.json()
+  const ol = document.querySelector("ol")
+  console.log(data_json)
+
+
+  for (let i of data_json){
+    const li = document.createElement("li")
+    li.innerText = ("name: ") + i.name + ("      points: ") + i.points
+    ol.appendChild(li)
+  }
+}
+
+playerdata()
